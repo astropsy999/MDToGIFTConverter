283d0806095987a72e75e3ef2d5b953c8241a16b
astropsy999 <bareust@gmail.com>
2024-03-03 00:38:56 +0300

    translate_gift_file, translate_text adjustment

--- main.py
+++ main.py
@@ -4,21 +4,23 @@ import re
 import sys
 from typing import Union, List, Optional
 
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QMessageBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QMessageBox, QInputDialog
 from google.cloud import translate_v2 as translate
 from google.oauth2 import service_account
+import subprocess
 
 credentials = service_account.Credentials.from_service_account_file(
     'handy-station-415405-198eaa66d90a.json')
 
 TO_TRANSLATE = False
+
+
 # TO_TRANSLATE = 'uk'
 
 # API_KEY = os.getenv('API_KEY')
 
 
-def translate_text(text: Union[str, bytes]) -> str:
+def translate_text(text: Union[str, bytes], target_language: str) -> str:
     # Init Google Cloud Translate API
     translate_client = translate.Client(credentials=credentials)
 
@@ -30,8 +32,8 @@ def translate_text(text: Union[str, bytes]) -> str:
 
     source_language = result['language']
 
-    if source_language != TO_TRANSLATE:
-        result = translate_client.translate(text, target_language=TO_TRANSLATE)
+    if source_language != target_language:
+        result = translate_client.translate(text, target_language=target_language)
         return result['translatedText']
     else:
         return text
@@ -66,12 +68,13 @@ def convert_to_gift(question_text: str) -> Optional[str]:
     question = question_match.group(2).strip()
 
     # Adjust the regular expression pattern to match all options at once
-    options = re.findall(r'- \[(.)\] (.+?)\n', question_match.group(3))
+    options = re.findall(r'- \[(.)\] (.+?)\n', question_text)
 
     # Convert the question and options to GIFT format
     gift_question = f"{question}" + " {"
+
     gift_options = ''.join(
-        [f"={'#' if is_correct == 'x' else ''}{html.escape(option)} " for is_correct, option in options])
+        [f"{'=' if is_correct == 'x' else '~'}{html.escape(option)} " for is_correct, option in options])
 
     return f"{gift_question}{gift_options.strip().replace('{', '&#123;').replace('}', '&#125;')}" + "}"
 
@@ -128,6 +131,33 @@ def convert_files_to_gift(md_files: List[str], current_directory: str) -> None:
                     f.write('\n\n')
 
 
+def translate_gift_file(directory: str, target_language: str) -> None:
+    # Get list of .gift files
+    gift_files = [f for f in os.listdir(directory) if f.endswith('.gift')]
+
+    for gift_file in gift_files:
+        # Read the content of the original file
+        with open(os.path.join(directory, gift_file), 'r', encoding='utf-8') as f:
+            original_content = f.read()
+            original_questions = original_content.split('\n\n')
+
+        # Translate each question and store them in a list
+        translated_questions = []
+        for question in original_questions:
+            translated_question = translate_text(question, target_language)
+            translated_questions.append(translated_question)
+
+        # Join translated questions into a single string with '\n\n' as separator
+        translated_content = '\n\n'.join(translated_questions)
+
+        # Create a new file for translated content
+        translated_filename = f"{target_language}_{gift_file}"
+        with open(os.path.join(directory, translated_filename), 'w', encoding='utf-8') as f:
+            f.write(translated_content)
+
+    QMessageBox.information(None, "Information", "Translation completed.")
+
+
 def main() -> None:
     app = QApplication(sys.argv)
 
@@ -140,11 +170,34 @@ def main() -> None:
 
     show_markdown_files_dialog(md_files)
 
-    reply = QMessageBox.question(None, "Confirmation", "Convert all files to GIFT format?", QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+    reply = QMessageBox.question(None, "Confirmation", "Convert all files to GIFT format?",
+                                 QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
 
     if reply == QMessageBox.StandardButton.Yes:
         convert_files_to_gift(md_files, current_directory)
-        QMessageBox.information(None, "Information", "Conversion completed.")
+        need_open = QMessageBox.information(None, "Information", "Conversion completed. Open folder?",
+                                            QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+
+        if need_open == QMessageBox.StandardButton.Yes:
+            # Open directory containing the converted files
+            subprocess.run(["open", current_directory])  # For macOS
+        elif need_open == QMessageBox.StandardButton.No:
+            # Ask user if they want to translate the file
+            translate_result = QMessageBox.question(None, "Information", "Would you like to translate the file?",
+                                                    QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+            if translate_result == QMessageBox.StandardButton.Yes:
+                language_options = {"Ukrainian": "uk", "Russian": "ru"}
+                # Show language selection dialog
+                language, ok_pressed = QInputDialog.getItem(None, "Select Language", "Select the target language:",
+                                                            list(language_options.keys()), 0, False)
+
+                if ok_pressed:
+                    language_code = language_options.get(language)
+                    if language_code:
+                        translate_gift_file(current_directory, language_code)
+                    else:
+                        QMessageBox.warning(None, "Warning", "Invalid language selection.")
+
     elif reply == QMessageBox.StandardButton.No:
         QMessageBox.information(None, "Information", "Conversion aborted.")
     else:
@@ -155,4 +208,3 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
-
